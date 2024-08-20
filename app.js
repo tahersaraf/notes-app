59ae1a69cd68957ad08e4f2d683cf4ee97f6ec45
Taher Saraf <tahersaraf98@gmail.com>
2024-08-20 23:50:59 +0530

    added dashboard and oauth

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const port = 3000 || process.env.PORT;
 
+app.use(
+  session({
+    secret: "blahblah",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Connect to DB
+connectDB();
 
 //Static Files
 app.use(express.static("public"));
@@ -18,6 +39,7 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 //Routes
+app.use("/", require("./server/routes/auth"));
 app.use("/", require("./server/routes/index"));
 app.use("/", require("./server/routes/dashboard"));
 
